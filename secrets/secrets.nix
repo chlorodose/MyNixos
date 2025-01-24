00b60de19244d11156ddce1a4079f8039d0e5ff3
chlorodose <chlorodose@chlorodose.me>
2025-01-24 21:41:28 +0800

    feat: add secrets

---
+++ secrets/secrets.nix
@@ -0,0 +1,9 @@
+let
+  cl-server =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDoG1njuBcqcylds7HIrSUUeqxiyJUUI+pduTVUJYXzr";
+in {
+  "cf-ddns.age".publicKeys = [ cl-server ];
+  "wg-private.age".publicKeys = [ cl-server ];
+  "wg-ps-phone.age".publicKeys = [ cl-server ];
+  "ppp-wan.age".publicKeys = [ cl-server ];
+}
