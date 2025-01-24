6e17b329fdda403dd9a81e8ba66f826a352c7531
chlorodose <chlorodose@chlorodose.me>
2025-01-24 21:14:17 +0800

    feat: add flake code

--- flake.nix
+++ flake.nix
@@ -2,12 +2,39 @@
   description = "My configuration for nixos";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    agenix = { # 密码管理
+      url = "github:ryantm/agenix/main";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+    impermanence = { # 管理状态
+      url = "github:nix-community/impermanence/master";
+    };
+
+    nixvim = { # 配置Neovim
+      url = "github:nix-community/nixvim/main";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs }@inputs: { };
+  outputs = { nixpkgs, home-manager, agenix, impermanence, nixvim }:
+    let
+      inputModules = [
+        home-manager.nixosModules.home-manager
+        agenix.nixosModules.default
+        impermanence.nixosModules.impermanence
+        { home-manager.sharedModules = [ nixvim.homeManagerModules.nixvim ]; }
+      ];
+    in { };
 }
