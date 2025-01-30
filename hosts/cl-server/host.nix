8d8a3583ed88018f6fda8d9c7598f22ea1a5be7b
chlorodose <chlorodose@chlorodose.me>
2025-01-30 17:19:25 +0800

    fix: enable service

--- hosts/cl-server/host.nix
+++ hosts/cl-server/host.nix
@@ -10,4 +10,9 @@
   system.fs.swap.num = 1;
   system.cpu = "intel";
   system.stateVersion = "25.05";
+  services.ddns.enable = true;
+  services.ups.enable = true;
+  services.postgres.enable = true;
+  services.gitHomeLab.enable = true;
+  services.mc.enable = true;
 }
