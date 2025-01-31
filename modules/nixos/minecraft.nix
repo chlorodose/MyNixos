8fda6d4155f58f04f6951d5f1b375caec87ef184
chlorodose <chlorodose@chlorodose.me>
2025-01-31 17:35:13 +0800

    fix: declear exporter config

--- modules/nixos/minecraft.nix
+++ modules/nixos/minecraft.nix
@@ -6,23 +6,27 @@
       enable = true;
       eula = true;
       dataDir = "/srv/minecraft";
-      servers.server = 
-      let 
+      servers.server = let
         modpack = pkgs.fetchPackwizModpack {
-          url = "https://github.com/chlorodose/my-mc-server/raw/v1.1.1/pack.toml";
+          url =
+            "https://github.com/chlorodose/my-mc-server/raw/v1.1.1/pack.toml";
           packHash = "sha256-5h9een1y6dpbFt61nSl8yNNZAedMH3kaQOpt3FG+Pss=";
-          manifestHash = "sha256:0bzg6slzdk6lblrh7lyyyrgymnkj3hpyl0n0dkdwf8mm0j1l87zz";
+          manifestHash =
+            "sha256:0bzg6slzdk6lblrh7lyyyrgymnkj3hpyl0n0dkdwf8mm0j1l87zz";
         };
         mcVersion = modpack.manifest.versions.minecraft;
         fabricVersion = modpack.manifest.versions.fabric;
-        serverVersion = lib.replaceStrings [ "." ] [ "_" ] "fabric-${mcVersion}";
+        serverVersion =
+          lib.replaceStrings [ "." ] [ "_" ] "fabric-${mcVersion}";
       in {
         enable = true;
         autoStart = true;
         openFirewall = true;
         restart = "always";
         enableReload = false;
-        package = pkgs.fabricServers.${serverVersion}.override { loaderVersion = fabricVersion; };
+        package = pkgs.fabricServers.${serverVersion}.override {
+          loaderVersion = fabricVersion;
+        };
         jvmOpts = "-Djava.net.useSystemProxies=true -Xms2048M -Xmx8192M";
         serverProperties = {
           difficulty = 3;
@@ -33,6 +37,11 @@
         };
         symlinks = {
           "mods" = "${modpack}/mods";
+        };
+        files = {
+          "config/exporter.properties" = {
+            value = { server-port = 8080; };
+          };
         };
       };
     };
