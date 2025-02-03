2c3237e2b4bdd086e8c40fd2820205cd1c32f6a8
chlorodose <chlorodose@chlorodose.me>
2025-02-03 10:16:46 +0800

    fix: revert removed neovim plugins

--- modules/home-manager/neovim/neovim.nix
+++ modules/home-manager/neovim/neovim.nix
@@ -10,5 +10,30 @@
     colorscheme = "catppuccin-mocha";
     impureRtp = false;
     wrapRc = true;
+    colorschemes.catppuccin.enable = true;
+    plugins.lsp = {
+      enable = true;
+      inlayHints = true;
+      servers = { nixd.enable = true; };
+    };
+    plugins.noice.enable = true;
+    plugins.blink-cmp.enable = true;
+    plugins.which-key.enable = true;
+    plugins.neo-tree.enable = true;
+    plugins.snacks.enable = true;
+    plugins.web-devicons.enable = true;
+    opts = {
+      termguicolors = true;
+      signcolumn = "yes";
+      colorcolumn = "80";
+      softtabstop = 4;
+      expandtab = true;
+      autoindent = true;
+      smartindent = true;
+      shiftwidth = 4;
+      shiftround = true;
+      number = true;
+      relativenumber = true;
+    };
   };
 }
