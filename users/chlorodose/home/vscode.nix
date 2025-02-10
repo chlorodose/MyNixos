e394a7f1e9649262dd2ac8ae5679feaff3aaaa5e
chlorodose <chlorodose@chlorodose.me>
2025-02-10 14:27:19 +0800

    add vscode

---
+++ users/chlorodose/home/vscode.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+    ];
+    userSettings = {
+
+    };
+    mutableExtensionsDir = false;
+  };
+}
