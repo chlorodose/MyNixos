6f7b34a1036bc4a5122df42809d112f7bf226f24
chlorodose <chlorodose@chlorodose.me>
2025-02-10 21:15:53 +0800

    fix theme

--- users/chlorodose/home/theme.nix
+++ users/chlorodose/home/theme.nix
@@ -1,14 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, osConfig, ... }: {
   qt = {
-    enable = true;
-    platformTheme = "gtk3";
+    enable = osConfig.modules.desktop.enable;
+    platformTheme.name = "gtk3";
     style = {
       package = pkgs.lightly-qt;
       name = "Lightly";
     };
   };
   gtk = {
-    enable = true;
+    enable = osConfig.modules.desktop.enable;
     theme = {
       package = pkgs.catppuccin-gtk;
       name = "catppuccin-mocha-mauve-standard+default";
