c44238a6f3186c6f6a8ee558102958bfc0aad5de
chlorodose <chlorodose@chlorodose.me>
2025-02-10 20:44:04 +0800

    add qt&gtk theme

---
+++ users/chlorodose/home/theme.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }: {
+  qt = {
+    enable = true;
+    platformTheme = "gtk3";
+    style = {
+      package = pkgs.lightly-qt;
+      name = "lightly";
+    };
+  };
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.catppuccin-gtk;
+      name = "catppuccin-mocha-mauve-standard+default";
+    };
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus";
+    };
+    cursorTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus";
+    };
+  };
+}
